8779b1aa9369e562967b28c27134c0d4ee581184
Naweed Akram <naweed@xgeno.com>
2021-01-21 20:20:15 +0300

    Commit
    Cimmit Files

---
+++ WeeklyXamarin.Android/MainApplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace WeeklyXamarin.Droid
+{
+    [Application(Theme = "@style/MainTheme")]
+    public class MainApplication : Application
+    {
+        public MainApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            //Xamarin.Essentials.Platform.Init(this);
+        }
+    }
+}
